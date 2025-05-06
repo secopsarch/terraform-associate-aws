675a2d71fc097c7bd4b3283e9418b5f65bce0f27
Desik@ <203578745+secopsarch@users.noreply.github.com>
2025-05-06 17:05:20 +0800

    lab-1 awstf

---
+++ projects/bucket.tf
@@ -0,0 +1,30 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.97.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
+  }
+}
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_s3_bucket" "labcontainer" {
+  bucket = "awslab-s3-arunbw"
+
+  tags = {
+    Name        = "aws-lab"
+    Environment = "sandbox"
+  }
+}
