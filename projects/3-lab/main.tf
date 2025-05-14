83f8272643ecd6051431d05b6a8cacd41055c220
Desik@ <203578745+secopsarch@users.noreply.github.com>
2025-05-14 15:58:24 +0800

    data-block with AMI lab

--- projects/3-lab/main.tf
+++ projects/3-lab/main.tf
@@ -25,11 +25,34 @@ provider "aws" {
   region = var.aws_region
 
 }
-
 locals {
   server_name = "websubnet-${var.env}"
-  vpc_name = "mydemovpc-${var.env}"
+  vpc_name    = "mydemovpc-${var.env}"
 }
+data "aws_region" "last-used" {}
+data "aws_availability_zones" "available-az" {}
+
+data "aws_ami" "websever" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "name"
+    values = [ "al2023-ami-2023.*-arm64" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "arm64" ]
+  }
+  filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+    name = "root-device-type"
+    values = [ "ebs" ]
+  }
+}
+
 resource "aws_vpc" "lab_vpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
@@ -37,13 +60,32 @@ resource "aws_vpc" "lab_vpc" {
   tags = {
     Name      = local.vpc_name
     terraform = "true"
+    region    = data.aws_region.last-used.description
   }
 }
-resource "aws_subnet" "web-subnet" {
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.lab_vpc.id
+  cidr_block = var.private_cidr
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.lab_vpc.id
+  cidr_block = var.public_cidr
+}
+resource "aws_subnet" "web_subnet" {
   vpc_id     = aws_vpc.lab_vpc.id
   cidr_block = var.web_cidr
 
   tags = {
     Name = local.server_name
+  }
+}
+resource "aws_spot_instance_request" "webserver" {
+  ami = data.aws_ami.websever.id
+  instance_type = "t3.micro"
+  spot_price = "0.05"
+
+  tags = {
+    Name = "test-spot"
   }
 }
--- projects/3-lab/variables.tf
+++ projects/3-lab/variables.tf
@@ -19,6 +19,16 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.10.0.0/16"
 }
+variable "private_cidr" {
+  description = "default private cidr block defined"
+  type        = string
+  default     = "10.10.10.0/24"
+}
+variable "public_cidr" {
+  description = "default public cidr block defined"
+  type        = string
+  default     = "10.10.10.0/24"
+}
 variable "web_cidr" {
   description = "default web cidr block defined"
   type        = string
